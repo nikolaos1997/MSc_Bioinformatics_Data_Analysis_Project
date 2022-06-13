d115150bfdd82b6c5d87fd79be889f1837c5e429
nikolaos1997 <103045738+nikolaos1997@users.noreply.github.com>
2022-06-13 20:04:04 +0100

    Update Standardize_Text_from_PDF.py

--- Standardize_Text_from_PDF.py
+++ Standardize_Text_from_PDF.py
@@ -14,10 +14,10 @@ from pathlib import Path
     
     
 
-def fonts(pdf, granularity=False): # extract word format
+def fonts(pdf): # extract word format
    
     styles = {}
-    font_counts = {}
+    format_count = {}
     
     doc = fitz.open(pdf)
 
@@ -27,25 +27,23 @@ def fonts(pdf, granularity=False): # extract word format
             if b['type'] == 0:  # block contains text
                 for l in b["lines"]:  # iterate through the text lines
                     for s in l["spans"]:  # iterate through the text spans
-                        identifier = "{0}".format(s['size'])
-                        styles[identifier] = {'size': s['size'], 'font': s['font']}
+                        format_id = "{0}".format(s['size'])
+                        styles[format_id] = {'size': s['size'], 'font': s['font']}
 
-                        font_counts[identifier] = font_counts.get(identifier, 0) + 1  # count the fonts usage
+                        format_count[format_id] = format_count.get(format_id, 0) + 1  # count the fonts usage
 
-    font_counts = sorted(font_counts.items(), key=itemgetter(1), reverse=True)
-
-    
-    return font_counts, styles
+    format_count = sorted(format_count.items(), key=itemgetter(1), reverse=True)
+    return format_count, styles
 
 
-def font_tags(font_counts, styles): #get dictionary with font sizes as keys and tags as value
+def font_tags(format_count, styles): #get dictionary with font sizes as keys and tags as value
    
-    p_style = styles[font_counts[0][0]]  # get style for most used font by count (paragraph)
-    p_size = p_style['size']  # get the paragraph's size
+    para_style = styles[format_count[0][0]]  # get style for most used font by count (paragraph)
+    para_size = para_style['size']  # get the paragraph's size
 
     # sorting the font sizes high to low
     font_sizes = []
-    for (font_size, count) in font_counts:
+    for (font_size, count) in format_count:
         font_sizes.append(float(font_size))
     font_sizes.sort(reverse=True)
 
@@ -54,13 +52,13 @@ def font_tags(font_counts, styles): #get dictionary with font sizes as keys and 
     size_tag = {}
     for size in font_sizes:
         idx += 1
-        if size == p_size:
+        if size == para_size:
             idx = 0
-            size_tag[size] = '<p>'
-        if size > p_size:
-            size_tag[size] = '<h{0}>'.format(idx)
-        elif size < p_size:
-            size_tag[size] = '<s{0}>'.format(idx)
+            size_tag[size] = '<p>' # for paragraphs
+        if size > para_size:
+            size_tag[size] = '<h{0}>'.format(idx) # for headers
+        elif size < para_size:
+            size_tag[size] = '<s{0}>'.format(idx) # for subscripts, which was not used eventually in the standardization 
 
     return size_tag
 
@@ -71,7 +69,7 @@ def headers_para(pdf, size_tag): #Scrapes headers & paragraphs from PDF and retu
     first = True  # boolean operator for first header
     previous_s = {}  # previous span
     
-    doc = fitz.open(pdf)
+    doc = fitz.open(pdf) #PyMuPDF
 
     for page in doc:
         blocks = page.getText("dict")["blocks"]
@@ -89,9 +87,6 @@ def headers_para(pdf, size_tag): #Scrapes headers & paragraphs from PDF and retu
                             else:
                                 if s['size'] == previous_s['size']:
 
-                                    if block_string and all((c == "|") for c in block_string):
-                                        # block_string only contains pipes
-                                        block_string = size_tag[s['size']] + s['text']
                                     if block_string == "":
                                         # new block has started, so append size tag
                                         block_string = size_tag[s['size']] + s['text']
@@ -108,54 +103,73 @@ def headers_para(pdf, size_tag): #Scrapes headers & paragraphs from PDF and retu
                 header_para.append(block_string)
     return header_para
 
-def text_stand(doc): #standardize text considering the previous
+def text_stand(pdf): 
 
 
-    f = fonts(doc, granularity=False)
-    head = headers_para(doc, font_tags(f[0], f[1]))
+    f = fonts(pdf)
+    head = headers_para(pdf, font_tags(f[0], f[1]))
 
     clean = []
-    for i in head:
+    for i in head: # clean text from characters that were used to distinguish blocks 
         i = i.replace('-| ', '')
         i = i.replace('|', '')
         i = i.replace('  ', ' ')
         clean.append(i)
 
-
-    #lista = ['Abstract','Introduction','Methods','Method', 'Results', 'Discussion','Conclusion','Conclusions', 'Materials', 'References', 'Reference List', 'Supplementary Material'] ## this can be used for the main text
+     # use the list as reference to detect potential headings in the PDF text
     lista_sup = ['Quality Control','Clinical Perspective ','QA/QC Results','QA/QC Methods','Supplementary Figure','ANIMAL STUDIES','HUMAN STUDIES','Supplemental References','Genotyping Methods','Supplement','Quality Control','Genome wide association genotyping','Supplementary Material','Supporting Methods','Supplementary Methods','References','Supplementary Figure','Study Subjects','Statistical methods','Supplemental Tables','Supplementary Materials','Supplementary Tables','Supplemental Figures','Supplementary Information', 'Electronic supplementary material']
     txt_dic = {} # use the above lista to map the words and detect the tags that are given to the headings in each different pdf
     
-    for i,j in enumerate(clean):
+    heading_tag_gen = [] #use the heading tag that is found from headings that are included in the lista_sup, to potentially detect heading that are not included in the list
+    key_gen = [] # the heading name that will be used as key for the dictionary, with value the text of the paragraph
+
+    for i,j in enumerate(clean): # get heading that are included in the lista_sup
             words = list(j.split())
             if len(words) != 0:
                 try:
-                    section = words[0] + ' ' + words[1] + ' ' + words[2]
-                except: section = words[0] + ' ' + words[1]
-                for k in lista:
-                    try:
-                        if search(k.lower(),section.lower()) and words[0].startswith('<'): 
-                            heading_tag = words[0][0:4] 
-                            key = words[0][3:] + ' ' + words[1]
-                    except: 
-                        if search(k.lower(),words[0].lower()) and words[0].startswith('<'): 
-                            heading_tag = words[0][0:4] 
-                            key = words[0][3:] 
+                    section = words[0] + ' ' + words[1] # use the first words of the splitted text as header names
+                except: section = words[0] 
+                for k in lista_sup: # iterate through the reference list
+                    if search(k.lower(),section.lower()) and words[0].startswith('<'): 
+                        heading_tag = words[0][0:4] # that's the heading tag
+                        heading_tag_gen.append(heading_tag) # save it for later to detect headings that are not included in the lista_sup
                         try: 
-                            key = key + ' ' + words[1] + ' ' + words[2]
-                        except: continue
+                            key = words[0][3:] + ' ' + words[1] + ' ' + words[2] # the name of the heading that will be used as key to our dictionary
+                            key_gen.append(key) # save it for later   
+                        except: 
+                            key = words[0][3:] + ' ' + words[1]
+                            key_gen.append(key)
                         l = []
                         try: 
-                            while clean[i+1].startswith('<p>') and not clean[i+1].startswith(str(heading_tag)):
-                                    l.append(clean[i + 1])#[i + 1])
+                            while clean[i+1].startswith('<p>'): # get the paragraph
+                                    l.append(clean[i + 1])
                                     i = i + 1
                         except: continue
-                           
-                        txt_dic[key] = str(l)
+                        if len(l) != 0:
+                            txt_dic[key] = str(l)
                     else:
                         txt_dic['Unrecognized'] = clean
+                       
+    for i,j in enumerate(clean): # now let's get the headings that are not included in the lista_supp
+            words = list(j.split())
+            if len(words) != 0:
+                if words[0].startswith(str(heading_tag_gen[0])):
+                    try: 
+                        key = words[0][3:] + ' ' + words[1] + ' ' + words[2]
+                    except: 
+                        key = words[0][3:] 
+                    if key != key_gen[0]: # exclude the headings that are already found
+                        l = []
+                        try: 
+                            while clean[i+1].startswith('<p>'):
+                                    l.append(clean[i + 1])
+                                    i = i + 1
+                        except: continue
+                        if len(l) != 0 :
+                            txt_dic[key] = str(l)
+                
 
-    for key in txt_dic.keys():  #substitute tags with ""
+    for key in txt_dic.keys():  
         txt_dic[key] = re.sub('<p>', '',str(txt_dic[key]))
         txt_dic[key] = re.sub('<h0>', '',str(txt_dic[key]))
         txt_dic[key] = re.sub('<h1>', '',str(txt_dic[key]))
